68dae5c34bb8568db296d7be11bf93457047a8c8
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-08-23 09:49:41 +0200

    3.3 OK

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-  response.json(persons)
-})
-
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   const numberOfPersons = persons.length;
   const currentTime = new Date();
   const contentInfo = `<p>Phonebook has info for ${numberOfPersons} people</p>
-    <p>${currentTime}</p>`
+    <p>${currentTime}</p>`;
 
-  response.send(contentInfo)
-})
+  response.send(contentInfo);
+});
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  }
+  else {
+    response.status(404).end()
+  }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
