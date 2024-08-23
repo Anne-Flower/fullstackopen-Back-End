6842b9f1dc9491590c18d3dd2141c67e9ef402c5
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-08-23 15:15:12 +0200

    3.8 OK

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ let persons = [
   },
 ];
 
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
