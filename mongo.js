f39a4a4e343f4c78487bbf0547fa476e2b27acff
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-08-28 10:57:19 +0200

    3.12 OK

--- mongo.js
+++ mongo.js
@@ -7,6 +7,38 @@ mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
+
+    const args = process.argv;
+    const password = args[2];
+    const name = args[3];
+    const number = args[4];
+
+    const personSchema = new mongoose.Schema({
+      name: String,
+      number: String,
+    });
+
+    const Person = mongoose.model("Person", personSchema);
+
+    if (name && number) {
+      const person = new Person({
+        name: name,
+        number: number,
+      });
+
+      return person.save().then(() => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.connection.close();
+      });
+    } else {
+      Person.find({}).then((result) => {
+        console.log("phonebook:");
+        result.forEach((person) => {
+          console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+      });
+    }
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error.message);
